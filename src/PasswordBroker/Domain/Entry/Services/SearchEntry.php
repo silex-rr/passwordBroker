cdf04292fb759ccd655c47920b99f05341d1d0a6
Anton.b <46520333+silex-rr@users.noreply.github.com>
2024-01-17 12:18:37 +0300

    Refactor SearchEntry service for user access - user can find only that entries for which they have access to read

--- src/PasswordBroker/Domain/Entry/Services/SearchEntry.php
+++ src/PasswordBroker/Domain/Entry/Services/SearchEntry.php
@@ -2,19 +2,14 @@
 
 namespace PasswordBroker\Domain\Entry\Services;
 
-use Identity\Infrastructure\Criteria\CriteriaInEntryGroups;
-use Identity\Infrastructure\Criteria\CriteriaNameOrEmailContains;
-use Identity\Infrastructure\Criteria\CriteriaNotInEntryGroups;
-use Identity\Infrastructure\Repository\UserRepository;
+use Identity\Domain\User\Models\User;
 use Illuminate\Contracts\Pagination\Paginator;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
-use PasswordBroker\Domain\Entry\Models\Entry;
-use PasswordBroker\Domain\Entry\Models\EntryGroup;
 use PasswordBroker\Domain\Entry\Models\Fields\Field;
-use PasswordBroker\Domain\Entry\Models\Fields\Password;
+use PasswordBroker\Infrastructure\Criteria\CriteriaEntriesThatUserHasAccessToRead;
 use PasswordBroker\Infrastructure\Criteria\CriteriaEntryTitleOrEntryFieldTitleContains;
 use PasswordBroker\Infrastructure\Repositories\EntryRepository;
 
@@ -25,6 +20,7 @@ class SearchEntry implements ShouldQueue
     use SerializesModels;
 
     public function __construct(
+        protected User $user,
         protected string $query,
         protected int $perPage,
         protected int $page,
@@ -38,14 +34,40 @@ class SearchEntry implements ShouldQueue
         if (!empty($this->query)) {
             $entryRepository->pushCriteria(new CriteriaEntryTitleOrEntryFieldTitleContains($this->query));
         }
+        $entryRepository->pushCriteria(new CriteriaEntriesThatUserHasAccessToRead($this->user));
 
 //        $userRepository->applyCriteria();
 //        $userRepository->query()->dd();
         $withArr = Field::getRelatedForWith();
         $withArr[] = 'entryGroup';
         $entryRepository->query()->with($withArr);
+
+//        $userId = $this->user->user_id->getNativeValue();
+//
+//        $entryGroupUserClosure = fn($q) => $q->where('user_id', '=', $userId);
+//        $entryRepository->query()->whereHas('entryGroup.admins', $entryGroupUserClosure)
+//            ->orWhereHas('entryGroup.moderators', $entryGroupUserClosure)
+//            ->orWhereHas('entryGroup.members', $entryGroupUserClosure);
 //        $entryRepository->applyCriteria();
-//        $entryRepository->query()->dd();
+//        $builder = $entryRepository->query();
+//        $toSql = $builder->toSql();
+//        $bindings = $builder->getBindings();
+//        $all = $builder->get()->all();
+//        $result = DB::select('
+//            select password_broker_entries.*
+//            from password_broker_entries
+//
+//            inner join password_broker_entry_groups as group_t on group_t.entry_group_id = password_broker_entries.entry_id
+//            inner join password_broker_entry_group_user as group_u on group_u.entry_group_id = group_t.entry_group_id
+//            where
+//
+//                group_u.user_id = ?
+//                and password_broker_entries.deleted_at is null
+//            group by password_broker_entries.entry_id
+//        ', [$bindings[2]]);
+//        dd($all, $toSql, $bindings, $result,  Entry::where('title', 'aaaa')->with('entryGroup.admins')->first());
+        //->dd();
+
 
         return $entryRepository->paginate(perPage: $this->perPage, columns: ['*'], pageName: 'page', page: $this->page);
 
