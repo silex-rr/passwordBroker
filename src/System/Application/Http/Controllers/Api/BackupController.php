32996f695a97b54264c3a139b332c20cf10fe110
Anton.b <46520333+silex-rr@users.noreply.github.com>
2024-01-24 10:59:27 +0300

    Implement backup search in BackupController
    Added a new function 'index' in the BackupController that enables searching for backups. It uses a new 'BackupSearchRequest' to fetch the search query, pagination details, and passes these to the 'SearchBackups' service. This allows for an improvement in search functionality within the backup management area of the application.

--- src/System/Application/Http/Controllers/Api/BackupController.php
+++ src/System/Application/Http/Controllers/Api/BackupController.php
@@ -5,13 +5,22 @@ namespace System\Application\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Http\JsonResponse;
+use System\Application\Http\Requests\BackupSearchRequest;
 use System\Application\Services\BackupService;
 use System\Domain\Backup\Models\Backup;
 use System\Domain\Backup\Service\CreateBackup;
+use System\Domain\Backup\Service\SearchBackups;
 
 class BackupController extends Controller
 {
     use DispatchesJobs;
+
+    public function index(BackupSearchRequest $request): JsonResponse
+    {
+        return new JsonResponse($this->dispatchSync(new SearchBackups(
+            query: $request->getQuery(), perPage: $request->getPerPage(), page: $request->getPage()
+        )));
+    }
     public function store(BackupService $backupService): JsonResponse
     {
         $backup = $this->dispatchSync(new CreateBackup(backup: new Backup(), backupService: $backupService));
