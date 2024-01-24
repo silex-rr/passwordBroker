6b2f532ddea80bed5e0ffa0e4377b2b6fdd9d152
Anton.b <46520333+silex-rr@users.noreply.github.com>
2024-01-24 10:59:08 +0300

    Introduce 'can:perform-with-backups' middleware in api routes
    Applied 'can:perform-with-backups' middleware to all routes related to Backup and BackupSetting within the 'auth.sanctum.cookie' group. This ensures additional authorization check for these routes, enhancing application's security by adding a layer to control which users have permission to perform operations related to backups.

--- src/System/Application/Routes/api.php
+++ src/System/Application/Routes/api.php
@@ -4,12 +4,15 @@ use System\Application\Http\Controllers\Api\BackupController;
 use System\Application\Http\Controllers\Api\BackupSettingController;
 
 Route::middleware('auth.sanctum.cookie')->group(function (){
-    Route::get('/setting/backupSetting/{backupScheduleSetting}', [BackupSettingController::class, 'show'])
-        ->name('system_backup_setting');
-    Route::post('/setting/backupSetting/{backupScheduleSetting}', [BackupSettingController::class, 'store']);
 
-    Route::post('/backup', [BackupController::class, 'store'])
-        ->name('system_backups');
-    Route::get('/backup/{backup:backup_id}', [BackupController::class, 'show'])
-        ->name('system_backup');
+    Route::middleware('can:perform-with-backups')->group(static function () {
+        Route::get('/setting/backupSetting/{backupScheduleSetting}', [BackupSettingController::class, 'show'])
+            ->name('system_backup_setting');
+        Route::post('/setting/backupSetting/{backupScheduleSetting}', [BackupSettingController::class, 'store']);
+        Route::get('/backups', [BackupController::class, 'index'])
+            ->name('system_backups');
+        Route::post('/backups', [BackupController::class, 'store']);
+        Route::get('/backups/{backup:backup_id}', [BackupController::class, 'show'])
+            ->name('system_backup');
+    });
 });
