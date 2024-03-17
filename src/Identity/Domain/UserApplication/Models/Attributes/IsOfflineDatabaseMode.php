51f4472d03451e0a7c5e0b8cdbf516a18a9503f7
Anton.b <46520333+silex-rr@users.noreply.github.com>
2024-03-17 14:52:36 +0300

    PBT-1: OpenAPI attributes reworked for UserApplication

--- src/Identity/Domain/UserApplication/Models/Attributes/IsOfflineDatabaseMode.php
+++ src/Identity/Domain/UserApplication/Models/Attributes/IsOfflineDatabaseMode.php
@@ -3,7 +3,9 @@
 namespace Identity\Domain\UserApplication\Models\Attributes;
 
 use App\Models\Abstracts\AbstractValue;
+use OpenApi\Attributes\Schema;
 
+#[Schema(schema: "Identity_IsOfflineDatabaseMode", type: "boolean")]
 class IsOfflineDatabaseMode extends AbstractValue
 {
     public function __construct(bool $value)
