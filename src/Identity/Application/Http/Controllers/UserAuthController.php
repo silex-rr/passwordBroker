40c711e1b58244ff694761287227b5b873f3cdb8
Anton.b <46520333+silex-rr@users.noreply.github.com>
2024-03-23 16:21:15 +0300

    PBT-1: OpenAPI Identity domain is finished

--- src/Identity/Application/Http/Controllers/UserAuthController.php
+++ src/Identity/Application/Http/Controllers/UserAuthController.php
@@ -11,9 +11,66 @@ use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Session;
+use OpenApi\Attributes\Get;
+use OpenApi\Attributes\JsonContent;
+use OpenApi\Attributes\MediaType;
+use OpenApi\Attributes\Post;
+use OpenApi\Attributes\Property;
+use OpenApi\Attributes\RequestBody;
+use OpenApi\Attributes\Response;
+use OpenApi\Attributes\Schema;
 
 class UserAuthController extends Controller
 {
+    #[Post(
+        path: "/identity/api/login",
+        summary: "Authentication endpoint",
+        requestBody: new RequestBody(
+            content: new MediaType(
+                mediaType: "multipart/form-data",
+                schema: new Schema(ref: "#/components/schemas/Identity_LoginRequest",),
+            ),
+        ),
+        tags: ["Identity_UserAuthController"],
+        responses: [
+            new Response(
+                response: 200,
+                description: "Provide auth token if token_is_required was true",
+                content: new JsonContent(
+                    properties: [
+                        new Property(
+                            property: "message",
+                            type: "string",
+                            default: "Login successful",
+                        ),
+                        new Property(
+                            property: "token",
+                            description: "provided only if token_is_required was true",
+                            type: "string",
+                            nullable: true
+                        ),
+                        new Property(
+                            property: "user",
+                            ref: "#/components/schemas/Identity_User",
+                            description: "provided only if token_is_required was true",
+                            type: "object",
+                        ),
+                    ],
+                    type: "object",
+                ),
+            ),
+            new Response(
+                response: 401,
+                description: "Failed login attempt",
+                content: new JsonContent(
+                    properties: [
+                        new Property(property: "message", type: "string", default: "Invalid email or password"),
+                    ],
+                    type: "object",
+                ),
+            ),
+        ],
+    )]
     public function login(LoginRequest $request): JsonResponse
     {
         $credentials = $request->only('email', 'password');
@@ -39,6 +96,23 @@ class UserAuthController extends Controller
         return new JsonResponse(['message' => 'Invalid email or password'], 401);
     }
 
+    #[Get(
+        path: "/identity/api/logout",
+        summary: "Logout endpoint",
+        tags: ["Identity_UserAuthController"],
+        responses: [
+            new Response(
+                response: 200,
+                description: "Logged out successfully",
+                content: new JsonContent(
+                    properties: [
+                        new Property(property: "message", type: "string", default: "Logged Out",),
+                    ],
+                    type: "object",
+                ),
+            ),
+        ],
+    )]
     public function logout(Request $request): JsonResponse
     {
         Session::flush();
@@ -49,6 +123,31 @@ class UserAuthController extends Controller
         $request->session()->regenerateToken();
         return new JsonResponse(['message' => 'Logged Out'], 200);
     }
+
+    #[Post(
+        path: "/identity/api/token",
+        summary: "Get a token",
+        requestBody: new RequestBody(
+            content: new MediaType(
+                mediaType: "multipart/form-data",
+                schema: new Schema(ref: "#/components/schemas/Identity_GetOrCreateTokenRequest",),
+            ),
+        ),
+        tags: ["Identity_UserAuthController"],
+        responses: [
+            new Response(
+                response: 200,
+                description: "Auth Token and User object",
+                content: new JsonContent(
+                    properties: [
+                        new Property(property: "token", type: "string",),
+                        new Property(property: "user", ref: "#/components/schemas/Identity_User",),
+                    ],
+                    type: "object",
+                ),
+            ),
+        ],
+    )]
     public function getToken(GetOrCreateTokenRequest $request): JsonResponse
     {
 //        /**
@@ -72,6 +171,28 @@ class UserAuthController extends Controller
         return new JsonResponse(['token' => $token, 'user' => $user]);
     }
 
+    #[Get(
+        path: "/identity/api/me",
+        summary: "Return a logged in user",
+        tags: ["Identity_UserAuthController"],
+        responses: [
+            new Response(
+                response: 200,
+                description: "Logged in User or message guest|firstUser, firstUser - system does not have any registered user",
+                content: new JsonContent(
+                    properties: [
+                        new Property(
+                            property: "message",
+                            type: "string",
+                            enum: ["loggedIn", "guest", "firstUser"],
+                        ),
+                        new Property(property: "user", ref: "#/components/schemas/Identity_User", nullable: true,),
+                    ],
+                    type: "object",
+                ),
+            ),
+        ],
+    )]
     public function show(): JsonResponse
     {
         $response = [
--- src/Identity/Application/Http/Requests/LoginRequest.php
+++ src/Identity/Application/Http/Requests/LoginRequest.php
@@ -3,7 +3,19 @@
 namespace Identity\Application\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use OpenApi\Attributes\Property;
+use OpenApi\Attributes\Schema;
 
+#[Schema(
+    schema: "Identity_LoginRequest",
+    properties: [
+        new Property(property: "email", type: "string", format: "email", nullable: false,),
+        new Property(property: "password", type: "string", format: "password", nullable: false,),
+        new Property(property: "token_is_required", type: "boolean", nullable: true),
+        new Property(property: "token_name", type: "string", nullable: true),
+    ],
+    type: "object",
+)]
 class LoginRequest extends FormRequest
 {
     public function rules() : array
