a80faaadbd06793bf5342d77f5cc22b03e14d114
Anton.b <46520333+silex-rr@users.noreply.github.com>
2024-03-23 15:45:04 +0300

    PBT-1: OpenAPI UserController - all actions

--- src/Identity/Application/Http/Requests/RegisterUserRequest.php
+++ src/Identity/Application/Http/Requests/RegisterUserRequest.php
@@ -5,7 +5,26 @@ namespace Identity\Application\Http\Requests;
 use Identity\Domain\User\Models\User;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rules\Password;
+use OpenApi\Attributes\Property;
+use OpenApi\Attributes\Schema;
 
+#[Schema(
+    schema: "Identity_RegisterUserRequest",
+    properties: [
+        new Property(
+            property: "user",
+            properties: [
+                new Property(property: "email", type: "string", format: "email", nullable: false),
+                new Property(property: "name", type: "string", nullable: false),
+                new Property(property: "password", type: "string", format: "password", nullable: false),
+                new Property(property: "password_confirmation", type: "string", format: "password", nullable: false),
+                new Property(property: "master_password", type: "string", format: "password", nullable: false),
+                new Property(property: "master_password_confirmation", type: "string", format: "password", nullable: false),
+            ],
+        ),
+    ],
+    type: "object",
+)]
 class RegisterUserRequest extends FormRequest
 {
     public function authorize() : bool
--- src/Identity/Application/Http/Requests/UpdateUserRequest.php
+++ src/Identity/Application/Http/Requests/UpdateUserRequest.php
@@ -6,9 +6,26 @@ use Identity\Domain\User\Models\User;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
 use Illuminate\Validation\Rules\Password;
+use OpenApi\Attributes\Property;
+use OpenApi\Attributes\Schema;
 
+#[Schema(
+    schema: "Identity_UpdateUserRequest",
+    properties: [
+        new Property(property:"email", ref: "#/components/schemas/Identity_UpdateUserRequest_email"),
+        new Property(property:"username", ref: "#/components/schemas/Identity_UpdateUserRequest_username"),
+        new Property(property:"password", ref: "#/components/schemas/Identity_UpdateUserRequest_password"),
+    ],
+    type: "object",
+)]
 class UpdateUserRequest extends FormRequest
 {
+    #[Schema(schema: "Identity_UpdateUserRequest_email", type: "string", format: "email",)]
+    public string $email;
+    #[Schema(schema: "Identity_UpdateUserRequest_username", type: "string", nullable: true,)]
+    public ?string $username = '';
+    #[Schema(schema: "Identity_UpdateUserRequest_password", type: "string", format: "password", nullable: true,)]
+    public ?string $password = '';
 
     public function authorize(): bool
     {
