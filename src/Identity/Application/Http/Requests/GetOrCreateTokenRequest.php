2672619ca58ea9c9b33ceb982791bf61d3f58b22
Anton.b <46520333+silex-rr@users.noreply.github.com>
2024-03-23 16:21:15 +0300

    PBT-1: OpenAPI Identity domain is finished

--- src/Identity/Application/Http/Requests/GetOrCreateTokenRequest.php
+++ src/Identity/Application/Http/Requests/GetOrCreateTokenRequest.php
@@ -3,7 +3,16 @@
 namespace Identity\Application\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use OpenApi\Attributes\Property;
+use OpenApi\Attributes\Schema;
 
+#[Schema(
+    schema: "Identity_GetOrCreateTokenRequest",
+    properties: [
+        new Property(property: "token_name", type: "string", nullable: false,),
+    ],
+    type: "object",
+)]
 class GetOrCreateTokenRequest extends FormRequest
 {
     public function authorize() : bool
