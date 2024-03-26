13913638524cd268a0acb8bf24a7f88ac3c42f59
Anton.b <46520333+silex-rr@users.noreply.github.com>
2024-03-26 18:27:15 +0300

    PBT-1: OpenAPI PasswordBroker - EntrySearchController is finished

--- src/PasswordBroker/Application/Http/Controllers/Api/EntrySearchController.php
+++ src/PasswordBroker/Application/Http/Controllers/Api/EntrySearchController.php
@@ -3,11 +3,20 @@
 namespace PasswordBroker\Application\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use Identity\Domain\User\Models\User;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Auth;
+use OpenApi\Attributes\AdditionalProperties;
+use OpenApi\Attributes\Get;
+use OpenApi\Attributes\Items;
+use OpenApi\Attributes\JsonContent;
+use OpenApi\Attributes\Property;
+use OpenApi\Attributes\QueryParameter;
+use OpenApi\Attributes\Response;
+use OpenApi\Attributes\Schema;
 use PasswordBroker\Application\Http\Requests\EntrySearchRequest;
 use PasswordBroker\Domain\Entry\Services\SearchEntry;
 
@@ -16,10 +25,54 @@ class EntrySearchController extends Controller
 {
 
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    #[Get(
+        path: "/passwordBroker/api/entrySearch",
+        summary: "Get a List of users who belongs to that EntryGroup",
+        tags: ["PasswordBroker_EntrySearchController"],
+        parameters: [
+            new QueryParameter(name: "q", required: false, schema: new Schema(ref: "#/components/schemas/PasswordBroker_EntrySearchRequest_q"),),
+            new QueryParameter(name: "page", required: false, schema: new Schema(ref: "#/components/schemas/PasswordBroker_EntrySearchRequest_page"),),
+            new QueryParameter(name: "perPage", required: false, schema: new Schema(ref: "#/components/schemas/PasswordBroker_EntrySearchRequest_perPage"),),
+        ],
+        responses: [
+            new Response(
+                response: 200,
+                description: "Founded Entries",
+                content: new JsonContent(
+                    allOf: [
+                        new Schema(ref: "#/components/schemas/Common_Paginator",),
+                        new Schema(
+                            description: "Entry set as data type",
+                            properties: [
+                                new Property(
+                                    property: "data",
+                                    type: "array",
+                                    items: new Items(
+                                        allOf: [
+                                            new Schema(ref: "#/components/schemas/PasswordBroker_Entry",),
+                                            new AdditionalProperties(properties: [
+                                                new Property(property: "entryGroup", ref: "#/components/schemas/PasswordBroker_EntryGroup",),
+                                            ]),
+                                        ],
+                                    )
+                                )
+                            ],
+                            type: "object",
+                        )
+                    ]
+                ),
+            )
+        ]
+    )]
     public function index(EntrySearchRequest $request): JsonResponse
     {
+        /**
+         * @var User $user
+         */
+        $user = Auth::user();
         $searchEntry = new SearchEntry(
-            user: Auth::user(),
+            user: $user,
             query: $request->getQuery(),
             perPage: $request->getPerPage(),
             page: $request->getPage(),
--- src/PasswordBroker/Application/Http/Requests/EntrySearchRequest.php
+++ src/PasswordBroker/Application/Http/Requests/EntrySearchRequest.php
@@ -3,6 +3,7 @@
 namespace PasswordBroker\Application\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use OpenApi\Attributes\Schema;
 
 /**
  * @property string $q
@@ -37,15 +38,19 @@ class EntrySearchRequest extends FormRequest
         ];
     }
 
+    #[Schema(schema: "PasswordBroker_EntrySearchRequest_q", description: "Search query", type: "string", default: "",)]
     public function getQuery(): string
     {
         return $this->q ?? '';
     }
 
+    #[Schema(schema: "PasswordBroker_EntrySearchRequest_perPage", type: "integer", default: 15, minimum: 1)]
     public function getPerPage(): int
     {
         return $this->perPage ?? 15;
     }
+
+    #[Schema(schema: "PasswordBroker_EntrySearchRequest_page", type: "integer", default: 1, minimum: 1)]
     public function getPage(): int
     {
         return $this->page ?? 1;
